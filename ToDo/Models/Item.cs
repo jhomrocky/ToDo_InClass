9d572951cd063254be9119a04d80ece2a4340640
jhomrocky <jhomrocky@gmail.com>
2017-10-27 10:37:42 -0400

    Completed the list class

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,13 @@ namespace ToDo.Models
     {
         [Key]
         public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime ItemDueDate { get; set; }
+        public string ItemDetails { get; set; }
+        public bool IsDone { get; set; } //try to always use bool in positive form (IsDone, IsTrue, etc)
 
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
     }
 }
