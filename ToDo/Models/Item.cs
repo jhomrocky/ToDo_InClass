255ba8d571c828ea76080d670d8e43fc5ff0d540
jhomrocky <jhomrocky@gmail.com>
2017-10-27 10:25:07 -0400

    Completed the Item model

---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+
+    }
+}
